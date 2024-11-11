f31fda0b3aea8acb86ebe6548dbdd76354b2d51d
mohamed mrdoctor <mhamedfekry0@gmail.com>
2024-11-11 06:50:21 +0200

    Open Graph Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,28 @@ const cairo  = localFont({
   weight: "100 200 300 400 500 600 700 800  900",
 });
 
-export const metadata: Metadata = {
-  title: "Portofolio App",
+export const metadata = {
+  title: "Portfolio App",
   description: "Front End Developer Portfolio",
   keywords: "Front End Developer, Portfolio, Web Development",
+  openGraph: {
+    title: "Portfolio App",
+    description: "Front End Developer Portfolio",
+    images: [
+      {
+        url: "https://media.discordapp.net/attachments/668451315348930571/1305393871156416562/New_Bitmap_image_-_Copy.png?ex=6732de47&is=67318cc7&hm=9964ac1861f84cfb1b2da4a59273a11ce0c3b4359fd8011e29c3acdfe45d090e", // استبدل بالرابط الصحيح
+        width: 800,
+        height: 600,
+        alt: "Portfolio Preview Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio App",
+    description: "Front End Developer Portfolio",
+    images: ["https://media.discordapp.net/attachments/668451315348930571/1305393871156416562/New_Bitmap_image_-_Copy.png?ex=6732de47&is=67318cc7&hm=9964ac1861f84cfb1b2da4a59273a11ce0c3b4359fd8011e29c3acdfe45d090e"], // استبدل بالرابط الصحيح
+  },
 };
 
 export default function RootLayout({
